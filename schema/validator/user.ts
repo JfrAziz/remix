0f0edb280a2ac2a8b998cf2508f1695d3f6f0c9b
Jafar Aziz <aziz@jafaraziz.com>
2024-02-24 17:58:18 +0700

    fix: make validator satisfies to schema

--- schema/validator/user.ts
+++ schema/validator/user.ts
@@ -1,28 +1,22 @@
-import {
-  Output,
-  email,
-  minLength,
-  object,
-  optional,
-  regex,
-  string,
-  toTrimmed,
-} from "valibot";
+import * as v from "valibot";
+import { User } from "schema";
 
-export type UpdateUserSchema = Output<typeof updateUserSchema>;
+export type UpdateUserSchema = v.Output<typeof updateUserSchema>;
 
-export const updateUserSchema = object({
-  email: optional(string([email("email not valid")])),
-  full_name: optional(
-    string([toTrimmed(), minLength(2, "full name at least 2 characters")])
+export const updateUserSchema = v.object({
+  email: v.optional(v.string([v.email("email not valid")])),
+  full_name: v.optional(
+    v.string([v.toTrimmed(), v.minLength(2, "full name at least 2 characters")])
   ),
-  user_name: optional(
-    string([
-      toTrimmed(),
-      regex(
+  user_name: v.optional(
+    v.string([
+      v.toTrimmed(),
+      v.regex(
         /^[a-zA-Z0-9]{3,16}$/,
         "username only alphanumeric and be between 3 and 16 characters in length"
       ),
     ])
   ),
-})
+}) satisfies v.BaseSchema<
+  Partial<Pick<User, "email" | "user_name" | "full_name">>
+>;
