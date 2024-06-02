5b0978db7f3b2ead8fff35fdf129b0db3bfc6ec8
Jafar Aziz <aziz@jafaraziz.com>
2024-06-03 01:37:11 +0700

    chore: remove duplicate vite config

--- vite.config.ts
+++ vite.config.ts
@@ -1,4 +1,4 @@
-import { defineConfig } from "vite"
+import { defineConfig } from "vitest/config"
 import tsconfigPaths from "vite-tsconfig-paths"
 import { vitePlugin as remix } from "@remix-run/dev"
 import devServer, { defaultOptions } from "@hono/vite-dev-server"
@@ -7,6 +7,14 @@ export default defineConfig({
   server: {
     hmr: {
       port: 3555,
+    },
+  },
+  test: {
+    reporters: ["verbose", "default"],
+    coverage: {
+      exclude: ["build"],
+      provider: "istanbul",
+      reporter: ["text", "html", "json-summary", "json", "cobertura"],
     },
   },
   plugins: [
